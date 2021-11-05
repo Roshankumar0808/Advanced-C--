fbaa3d1dbbf85fef9da5c90edb85cefdebfce816
Roshankumar0808 <roshananand2014@gmail.com>
2021-11-05 20:29:55 +0530

    second commit

---
+++ varcomment3.cpp
@@ -0,0 +1,14 @@
+// This is single line comment
+/*
+This is 
+mlti
+line 
+comments*/
+
+#include<iostream>
+using namespace std;
+int main(){
+    int sum=6; // This is integer variable "sum"
+    cout<<"Hello World"<< sum; // This print sum value along with hello world. coz its in cout.
+    return 0;
+}
---
+++ variable4.cpp
@@ -0,0 +1,13 @@
+//1
+#include<iostream>
+using namespace std;
+int main(){
+    int a=4,b=6;
+    char c ='d';
+    cout<<"The value of a is:"<<a<<".\nThe value of b is: "<<b;
+    cout<<"\nThe value of c is "<<c;
+    return 0;
+}
+
+/* "\n" it acts as a line break.*/
+
