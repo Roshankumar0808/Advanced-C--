7879efefd1a366c7412e37f71e8983db56b7b5d5
Roshankumar0808 <roshananand2014@gmail.com>
2021-11-05 19:25:01 +0530

    first committ

---
+++ basic2.cpp
@@ -0,0 +1,20 @@
+//1
+#include<iostream>
+int main(){
+    std::cout<<"Hello World";
+    return 0;
+}
+
+//2
+#include<iostream>
+using namespace std;
+int main(){
+    cout<<"Hello World";
+    return 0;
+}
+
+/*There is difference in program 1 and 2 in program 1 we are using "std" which means in standard library there
+is something called cout so use it from there.
+
+But in code2 we are using we are first inserting "std" which means now evertime we do not need to insert std
+before using any of its contents.*/
---
+++ installation.cpp
@@ -0,0 +1,8 @@
+/* This lecture basically talks aabout installation of vs code and g++ 
+   and tells how to compile and run our program in compiler*/
+   
+#include<iostream>
+int main(){
+    std::cout<<"Hello World";
+    return 0;
+}
